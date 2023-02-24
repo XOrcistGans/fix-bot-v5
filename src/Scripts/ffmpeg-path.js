fd27d12d8f2243964cd94cd0be2bdcd14777d047
XOrcistGans <112866322+XOrcistGans@users.noreply.github.com>
2023-02-24 18:09:39 +0700

    Create ffmpeg-path.js

---
+++ src/Scripts/ffmpeg-path.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const path = require('path');
+
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+
+const ffmpegDir = path.dirname(ffmpegPath);
+
+console.log(`${ffmpegDir}`);
